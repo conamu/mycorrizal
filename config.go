d2999d184d180756d0045f8263e0058d96a71e2a
conamu <constantin.amundsen@ho-me.zone>
2025-09-22 13:26:30 +0200

    base configguration and setup of main struct

--- config.go
+++ config.go
@@ -1,18 +1,76 @@
 package mycorrizal
 
 import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"log/slog"
+	"net"
+	"net/url"
 	"os"
 )
 
+const (
+	// DC_MODE_DNS_SD uses dns service discovery
+	DC_MODE_DNS_SD = iota
+	// DC_MODE_CONSUL uses Consul service API
+	DC_MODE_CONSUL
+	// DC_MODE_STATIC uses a static list of Addresses
+	DC_MODE_STATIC
+)
+
 type Config struct {
-	logger *slog.Logger
+	Ctx    context.Context
+	Logger *slog.Logger
+	/*
+		DiscoveryMode can be one of
+
+		Use dns service discovery from DiscoveryHost
+		DC_MODE_DNS_SD
+
+		Use Consul service API from DiscoveryHost
+		DC_MODE_CONSUL
+
+		Use a static list of Addresses from NodeAddrs
+		DC_MODE_STATIC
+	*/
+	DiscoveryMode int
+	/*
+		DiscoveryHost is either the DNS service or the Consul API endpoint used.
+		Specify "url:port" or "ip:port"
+	*/
+	DiscoveryHost *url.URL
+	/*
+		NodeAddrs specifies a static list of servers to connect to
+		Mycorrizal will generally exclude connecting to its own network Address,
+		so it's safe to include a complete list of all node addresses
+	*/
+	NodeAddrs []net.TCPAddr
+	/*
+		HttpClientTLSEnabled if true, supply HttpClientTLSCACert and HttpClientTLSCert
+		to authenticate with Consul API
+		Only needed if DiscoveryMode is set to DC_MODE_CONSUL
+	*/
+	HttpClientTLSEnabled bool
+	HttpClientTLSCACert  *x509.CertPool
+	HttpClientTLSCert    *tls.Certificate
+	/*
+		SingleMode disables all Cluster features but leaves the listener enabled for the CLI.
+		A node in SingleMode will reject all connections besides ones identified as an Authenticated CLI instance.
+	*/
+	SingleMode bool
+	ListenPort int
 }
 
 func GetDefaultConfig() *Config {
+
 	return &Config{
-		logger: slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Ctx: context.Background(),
+		Logger: slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 			Level: slog.LevelInfo,
 		})),
+		DiscoveryMode: DC_MODE_STATIC,
+		SingleMode:    false,
+		ListenPort:    6969,
 	}
 }
--- mycorrizal.go
+++ mycorrizal.go
@@ -1,22 +1,115 @@
 package mycorrizal
 
-import "log/slog"
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Mycorrizal interface {
 	Start() error
 }
 
-type mycorrizal struct{
-	l *slog.Logger
+type mycorrizal struct {
+	nodeId        string
+	ctx           context.Context
+	logger        *slog.Logger
+	httpClient    *http.Client
+	connReg       *connectionRegistry
+	discoveryMode int
+	nodeAddrs     []net.TCPAddr
+	singleMode    bool
 }
 
 func New(cfg *Config) (Mycorrizal, error) {
+	ctx := cfg.Ctx
+
+	id := os.Getenv("MYCORRIZAL_ID")
+
+	if id == "" {
+		// Use the IDs of env variable to enable
+		// having the same IDs as the containers in the
+		// Orchestrator for better visibility or generate own IDs
+		id = uuid.NewString()
+	}
+
+	var httpClient *http.Client
+	if cfg.DiscoveryMode == DC_MODE_CONSUL {
+		var tlsConfig *tls.Config
+		if cfg.HttpClientTLSEnabled {
+			if cfg.HttpClientTLSCACert == nil || cfg.HttpClientTLSCert == nil {
+				return nil, errors.New("enabling TLS requires setting HttpClientTLSCaCert and HttpClientTLSCert")
+			}
+
+			tlsConfig = &tls.Config{
+				RootCAs:      cfg.HttpClientTLSCACert,
+				Certificates: []tls.Certificate{*cfg.HttpClientTLSCert},
+			}
+		}
+
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				MaxIdleConnsPerHost: 1,
+				MaxIdleConns:        1,
+				IdleConnTimeout:     5 * time.Second,
+				TLSClientConfig:     tlsConfig,
+			},
+		}
+
+	}
+
+	if cfg.DiscoveryMode == DC_MODE_STATIC && cfg.NodeAddrs == nil {
+		return nil, errors.New("static discovery mode reuires NodeAddrs to be set")
+	}
+
+	if cfg.DiscoveryMode == DC_MODE_STATIC && len(cfg.NodeAddrs) == 0 {
+		cfg.Logger.Warn("running in static discovery mode but found no addresses in NodeAddrs array")
+	}
+
+	if cfg.DiscoveryMode == DC_MODE_CONSUL || cfg.DiscoveryMode == DC_MODE_DNS_SD && cfg.DiscoveryHost == nil {
+		return nil, errors.New("discovery modes consul and DNS Service discovery need discoveryHost to be set")
+	}
+
+	if cfg.SingleMode {
+		cfg.Logger.Info("Node running in single mode, no Cluster connections")
+	}
+
 	return &mycorrizal{
-		l: cfg.logger,
+		nodeId:        id,
+		ctx:           ctx,
+		logger:        cfg.Logger,
+		httpClient:    httpClient,
+		connReg:       newConnectionRegistry(),
+		discoveryMode: cfg.DiscoveryMode,
+		nodeAddrs:     cfg.NodeAddrs,
+		singleMode:    cfg.SingleMode,
 	}, nil
 }
 
 func (mc *mycorrizal) Start() error {
-	mc.l.Info("mycorrizal started")
+	mc.logger.Info("mycorrizal started")
 	return nil
+}
+
+// connectionRegistry is needed to keep track of connections and merge connections for efficiency
+type connectionRegistry struct {
+	mu       sync.Mutex
+	outbound map[string]*net.TCPConn
+	inbound  map[string]*net.TCPConn
+}
+
+func newConnectionRegistry() *connectionRegistry {
+	return &connectionRegistry{
+		mu:       sync.Mutex{},
+		outbound: make(map[string]*net.TCPConn),
+		inbound:  make(map[string]*net.TCPConn),
+	}
 }
